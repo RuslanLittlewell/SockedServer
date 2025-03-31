f78b46a9bfde70eedfbfdb020db6382dd3c890f3
Jakub <frontend@mediatool.tv>
2025-03-31 14:28:18 +0200

    Add origin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://socked-front.vercel.app", "http://localhost:5173", "http://localhost:5174"],
     methods: ["GET", "POST"]
   }
 });
