3634f379d02ac860441671931fe4c02b6816f09b
Jakub <frontend@mediatool.tv>
2025-04-17 03:17:16 +0200

    Add static users

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { createServer } = require("http");
 const ws = require("socket.io");
+const { users } = require('./users');
 const cors = require("cors");
 
 const app = express();
@@ -37,7 +38,8 @@ io.on("connection", (socket) => {
     rooms[roomId] = {
       users: [],
       messages: [],
-      broadcasterOffer: null
+      broadcasterOffer: null,
+      privateMessages: {}
     };
   }
 
@@ -60,8 +62,29 @@ io.on("connection", (socket) => {
     username: user.username,
   });
 
+  io.to(roomId).emit('usersData', users);
+
   // Отправка истории сообщений новому пользователю
   socket.emit("messageHistory", rooms[roomId].messages);
+
+  socket.on("get-private-messages-history", ({ username }) => {
+    if(rooms[roomId].privateMessages[username]) {
+      socket.emit('send-private-message-history', rooms[roomId].privateMessages?.[username] || [])
+    }
+  })
+
+  socket.on("private-message", ({ username, message }) => {
+    const newMessage = {
+      ...message,
+      id: Date.now().toString(),
+      timestamp: new Date(),
+    };
+    if (!rooms[roomId].privateMessages[username]) {
+      rooms[roomId].privateMessages[username] = [];
+    }
+    rooms[roomId].privateMessages[username].push(newMessage);
+    io.to(roomId).emit("private-message", newMessage);
+  });
 
 
   socket.on("disconnect", () => {
@@ -125,12 +148,19 @@ io.on("connection", (socket) => {
   });
 
   socket.on("ask-private", ({roomId , username}) => {
+    if (!rooms[roomId].privateMessages[username]) {
+      rooms[roomId].privateMessages[username] = [];
+    }
     io.to(roomId).emit("private-request", { username })
   });
 
   socket.on("user-accept-private", ({ roomId }) => {
     io.to(roomId).emit("start-private");
   })
+
+  socket?.on("private-finished", ({ roomId }) => {
+    io.to(roomId).emit("private-finished");
+  });
 
   socket.on("delete-all-messages", ({ roomId }) => {
     if (rooms[roomId]) {
